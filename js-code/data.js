a10aa31d5831fdfb37ecfd951129d9e6850be47e
Anil Kumar <anilkumarsingh1911@gmail.com>
2022-01-26 23:59:01 +0530

    read js file

--- js-code/data.js
+++ js-code/data.js
@@ -1,11 +1,17 @@
 let studentData = [
-    { id: '1', name: 'anil' },
-    { id: '2', name: 'soni' },
-    { id: '3', name: 'moni' },
+    { id: 1, name: 'anil', age: 33 },
+    { id: 2, name: 'soni', age: 22 },
+    { id: 3, name: 'moni' },
 ];
 
 function show() {
+    let msg = '';
     for (let index = 0; index < studentData.length; index++) {
-        console.log(studentData[index].name);
+        console.log(studentData[index].id + ' - ' + studentData[index].name + ' - ' + studentData[index].age);
+
+        msg += '<br>' + studentData[index].id + ' - ' + studentData[index].name + ' - ' + studentData[index].age;
+        // document.writeln(msg);
     }
+    document.getElementById("demo").innerHTML = msg;
+
 }
