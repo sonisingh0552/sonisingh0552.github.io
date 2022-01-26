3b653c1eb4994e4c4b1e3a0d7d12cc220fa0e4e3
Anil Kumar <anilkumarsingh1911@gmail.com>
2022-01-26 16:07:20 +0530

    data file
    added

---
+++ js-code/data.js
@@ -0,0 +1,11 @@
+let studentData = [
+    { id: '1', name: 'anil' },
+    { id: '2', name: 'soni' },
+    { id: '3', name: 'moni' },
+];
+
+function show() {
+    for (let index = 0; index < studentData.length; index++) {
+        console.log(studentData[index].name);
+    }
+}
